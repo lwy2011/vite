ac70ea3e99c871e889cf06022d82811e773983fb
liuwanyong <liuwanyong2018@gmail.com>
2023-03-16 19:42:14 +0800

    add autoprefixer to fix css for broswer,vite.config.css.postcss.plugin设置的,不需要依赖postcss的

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, normalizePath } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
+import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 const variablePath= normalizePath(path.resolve('./src/variable.scss'))
 //normalizePath是解决win mac路径的不同的
@@ -17,6 +18,15 @@ export default defineConfig({
     modules:{
       generateScopedName:'[name]_[local]_[hash:base64:5]'
       //name 文件名 local 类名，自定义module哈希值
+    },
+    postcss:{
+      plugins:[
+        autoprefixer({
+          overrideBrowserslist:[
+            'Chrome > 40','ff > 31','ie 11'
+          ]
+        })
+      ]
     }
   }
 })
