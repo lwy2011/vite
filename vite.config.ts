8b4038550c9cca1befee2a26bda6d98b454cbd0c
liuwanyong <liuwanyong2018@gmail.com>
2023-03-16 14:52:00 +0800

    还原initiate初始状态

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root:path.join(__dirname,'src')
+  // root:path.join(__dirname,'src')
   //这里设置，手动设置index.html的入口路径为src目录下的
 })
