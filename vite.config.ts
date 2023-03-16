22408c678670a01b6657ed8f317223ee49695675
liuwanyong <liuwanyong2018@gmail.com>
2023-03-16 15:59:31 +0800

    use vite.config 的css设置,为每一个scss文件自动头部写入import 需要的文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, normalizePath } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
 // https://vitejs.dev/config/
+const variablePath= normalizePath(path.resolve('./src/variable.scss'))
+//normalizePath是解决win mac路径的不同的
 export default defineConfig({
   plugins: [react()],
+  //css
+  css:{
+    preprocessorOptions:{
+      scss:{
+        additionalData:`@import "${variablePath}";`
+        //自动为每一个scss文件开头注入
+      }
+    }
+  }
 })
