d433e2008a247092942112189bf24e3935db6caa
liuwanyong <liuwanyong2018@gmail.com>
2023-03-18 18:58:48 +0800

    add autoprefixer ,set vite.config.css.postcss.plugin.autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,26 @@ import { defineConfig, normalizePath } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 const variablePath = normalizePath(path.resolve('./src/variable.scss'))
+import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css:{
-    preprocessorOptions:{
-      scss:{
-        additionalData:`@import "${variablePath}";`
+   plugins: [react()],
+   css: {
+      preprocessorOptions: {
+         scss: {
+            additionalData: `@import "${variablePath}";`
+         }
+      },
+      modules: {
+         generateScopedName: '[name]_[local]_[hash:base64:5]'
+      },
+      postcss: {
+         plugins: [
+            autoprefixer({
+               // 指定目标浏览器
+               overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
+            })
+         ]
       }
-    },
-    modules:{
-      generateScopedName:'[name]_[local]_[hash:base64:5]'
-    }
-  }
+   }
 })
