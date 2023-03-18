d0ed72af9202aa983ebfec7d46302d90c63c50aa
liuwanyong <liuwanyong2018@gmail.com>
2023-03-18 18:43:39 +0800

    add variable.scss ,set vite.config.css,set globle import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, normalizePath } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path'
+const variablePath = normalizePath(path.resolve('./src/variable.scss'))
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css:{
+    preprocessorOptions:{
+      scss:{
+        additionalData:`@import "${variablePath}";`
+      }
+    }
+  }
 })
