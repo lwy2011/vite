f2b23d024021c70593c6ca9fc14f37108220ba5a
liuwanyong <liuwanyong2018@gmail.com>
2023-03-24 18:08:25 +0800

    fix 环境变量的注册,加入如何引用的注释

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  // 自定义的环境变量
+  readonly VITE_IMG_BASE_URL: string;
+}
+// new URL('./logo.png', import.meta.env.VITE_IMG_BASE_URL).href 这样访问环境变量
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
